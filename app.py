2f66b5a8f34a912f423e1ed39b5fd49ddeced059
Thaslim1604 <thaslimasulthana400@gmail.com>
2026-04-03 12:05:07 +0530

    fix port for render

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def stats():
         return jsonify(latest_stats)
 
 if __name__ == '__main__':
-    app.run(debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
